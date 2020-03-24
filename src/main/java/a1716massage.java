3299a4d72571148394131ec96f82eba121f83d45
liuwenhao <liuwenhao4423@163.com>
2020-03-24 21:28:08 +0800

    0324

--- src/main/java/a1716massage.java
+++ src/main/java/a1716massage.java
@@ -4,7 +4,7 @@ public class a1716massage {
         if(nums.length == 1) return nums[0];
         if(nums.length == 2) return Math.max(nums[0], nums[1]);
         int[] res = new int[nums.length];
-        res[0] = nums[0];res[1]=nums[1];
+        res[0] = nums[0];res[1]=Math.max(nums[1],nums[0]);
         for(int i=2; i < nums.length; i++){
             res[i] = Math.max(res[i - 1], nums[i] + res[i - 2]);
         }
---
+++ src/main/java/a945minIncrementForUnique.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class a945minIncrementForUnique {
+    public int minIncrementForUnique(int[] A) {
+        int[] ii;
+        for(int i=0; i<A.length; i++){
+            
+        }
+    }
+}
