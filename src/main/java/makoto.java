bd5f557a8e0a36efc3a7c9f646db40ba0256b3ea
makoto4423 <52313862+makoto4423@users.noreply.github.com>
2020-04-01 09:34:26 +0800

    0401

--- src/main/java/makoto.java
+++ src/main/java/makoto.java
@@ -4,8 +4,8 @@ import happyNum.happy;
 public class makoto {
 
     public static void main(String[] args){
-        a1716massage is = new a1716massage();
-        int[] arr = {2,1,4,5,3,1,1,3};
-        System.out.println(is.massage(arr));
+        a66plusOne is = new a66plusOne();
+        int[] arr = {1,2,4};
+        System.out.println(is.plusOne(arr));
     }
 }
