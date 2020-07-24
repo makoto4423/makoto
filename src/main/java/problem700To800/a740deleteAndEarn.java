f28e251ebec42ab4caf41a670fbfe6121dfaee3d
makoto4423 <52313862+makoto4423@users.noreply.github.com>
2020-07-24 14:15:25 +0800

    0724

--- src/main/java/problem700To800/a740deleteAndEarn.java
+++ src/main/java/problem700To800/a740deleteAndEarn.java
@@ -1,10 +1,23 @@
 package problem700To800;
 
-import java.util.Arrays;
-
+@Deprecated
 public class a740deleteAndEarn {
     public int deleteAndEarn(int[] nums) {
-        Arrays.sort(nums);
-        return 0;
+        int[] count = new int[10001];
+        for (int x : nums) count[x]++;
+        int avoid = 0, using = 0, prev = -1;
+
+        for (int k = 0; k <= 10000; ++k)
+            if (count[k] > 0) {
+                int m = Math.max(avoid, using);
+                if (k - 1 != prev) {
+                    using = k * count[k] + m;
+                } else {
+                    using = k * count[k] + avoid;
+                }
+                avoid = m;
+                prev = k;
+            }
+        return Math.max(avoid, using);
     }
 }
