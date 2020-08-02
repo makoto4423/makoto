ee10a86aa2eb0aa17f28e8a787580a52f01d542c
liuwenhao <liuwenhao4423@163.com>
2020-08-02 13:57:45 +0800

    0802

---
+++ src/main/java/problem100To200/a114flatten.java
@@ -0,0 +1,34 @@
+package problem100To200;
+
+import struct.TreeNode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+@Deprecated
+public class a114flatten {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        stack.push(root);
+        TreeNode prev = null;
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.pop();
+            if (prev != null) {
+                prev.left = null;
+                prev.right = curr;
+            }
+            TreeNode left = curr.left, right = curr.right;
+            if (right != null) {
+                stack.push(right);
+            }
+            if (left != null) {
+                stack.push(left);
+            }
+            prev = curr;
+        }
+    }
+
+}
