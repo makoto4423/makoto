4b98c40cd582140693bef86541d7678c91104db1
makoto4423 <52313862+makoto4423@users.noreply.github.com>
2020-09-14 10:27:52 +0800

    0914

--- src/main/java/problem1To100/a94inorderTraversal.java
+++ src/main/java/problem1To100/a94inorderTraversal.java
@@ -5,23 +5,40 @@ import struct.TreeNode;
 import java.util.ArrayList;
 import java.util.List;
 
+// Morris遍历算法,空间复杂度为O(1)
 public class a94inorderTraversal {
-    List<Integer> res;
 
     public List<Integer> inorderTraversal(TreeNode root) {
-        res = new ArrayList<>();
-        if(root == null)    return res;
-        recur(root);
+        List<Integer> res = new ArrayList<Integer>();
+        TreeNode predecessor = null;
+
+        while (root != null) {
+            if (root.left != null) {
+                // predecessor 节点就是当前 root 节点向左走一步，然后一直向右走至无法走为止
+                predecessor = root.left;
+                while (predecessor.right != null && predecessor.right != root) {
+                    predecessor = predecessor.right;
+                }
+
+                // 让 predecessor 的右指针指向 root，继续遍历左子树
+                if (predecessor.right == null) {
+                    predecessor.right = root;
+                    root = root.left;
+                }
+                // 说明左子树已经访问完了，我们需要断开链接
+                else {
+                    res.add(root.val);
+                    predecessor.right = null;
+                    root = root.right;
+                }
+            }
+            // 如果没有左孩子，则直接访问右孩子
+            else {
+                res.add(root.val);
+                root = root.right;
+            }
+        }
         return res;
     }
 
-    public void recur(TreeNode node) {
-        if (node.left != null) {
-            recur(node.left);
-        }
-        res.add(node.val);
-        if (node.right != null) {
-            recur(node.right);
-        }
-    }
 }
