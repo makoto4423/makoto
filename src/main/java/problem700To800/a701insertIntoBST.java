43cb3305f18680dd5347e08872196dda9d031286
liuwenhao <liuwenhao4423@163.com>
2020-09-30 00:10:22 +0800

    0930

---
+++ src/main/java/problem700To800/a701insertIntoBST.java
@@ -0,0 +1,15 @@
+package problem700To800;
+
+import struct.TreeNode;
+
+public class a701insertIntoBST {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        if (root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+}
