632f36f9c79dbae61bebaac355e786ea60d791c8
liuwenhao <liuwenhao4423@163.com>
2020-10-08 10:23:42 +0800

    1008

--- src/main/java/problem300To400/a344reverseString.java
+++ src/main/java/problem300To400/a344reverseString.java
@@ -1,8 +1,11 @@
 package problem300To400;
 
+/**
+ * 6个月前通过了，现在不通过。。。
+ */
 public class a344reverseString {
     public void reverseString(char[] s) {
-        for(int i=0; i<s.length/2-1; i++){
+        for(int i=0; i<=s.length/2-1; i++){
             char temp = s[i];
             s[i] = s[s.length-i-1];
             s[s.length-i-1] = temp;
