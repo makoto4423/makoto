994bba73fe88d9c7c1bd6e885066a077c32a7183
makoto4423 <52313862+makoto4423@users.noreply.github.com>
2020-12-09 09:43:52 +0800

    1209

---
+++ src/main/java/problem1To100/a62uniquePaths.java
@@ -0,0 +1,20 @@
+package problem1To100;
+
+public class a62uniquePaths {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) continue;
+                if (i != 0) {
+                    dp[i][j] += dp[i - 1][j];
+                }
+                if (j != 0) {
+                    dp[i][j] += dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
