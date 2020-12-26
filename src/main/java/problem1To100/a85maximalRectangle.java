cb21bb5c621a236241a5420d37f3e3e06a916e82
liuwenhao <liuwenhao@agree.com.cn>
2020-12-26 21:41:11 +0800

    1226

---
+++ src/main/java/problem1To100/a85maximalRectangle.java
@@ -0,0 +1,33 @@
+package problem1To100;
+
+@Deprecated
+public class a85maximalRectangle {
+    public int maximalRectangle(char[][] matrix) {
+        int m = matrix.length;
+        if (m == 0) return 0;
+        int n = matrix[0].length;
+        int[][] left = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    left[i][j] = (j == 0 ? 0 : left[i][j - 1]) + 1;
+                }
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    int width = left[i][j];
+                    int area = width;
+                    for (int k = i - 1; k >= 0; k--) {
+                        width = Math.min(width, left[k][j]);
+                        area = Math.max(area, (i - k + 1) * width);
+                    }
+                    res = Math.max(res, area);
+                }
+            }
+        }
+        return res;
+    }
+}
