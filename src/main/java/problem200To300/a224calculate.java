84eaa4b94836bf7eb30fd59a73ae062bf49dddd1
makoto4423 <52313862+makoto4423@users.noreply.github.com>
2021-03-11 16:52:19 +0800

    0311

--- src/main/java/problem200To300/a224calculate.java
+++ src/main/java/problem200To300/a224calculate.java
@@ -5,29 +5,62 @@ import java.util.*;
 public class a224calculate {
     public int calculate(String s) {
         s = s.replaceAll(" ", "");
-        List<String> queue = new ArrayList<>();
-        int pre = 0;
-        for(int i=0;i<s.length();i++){
-            char c = s.charAt(i);
-            if(c == '(' || c == ')'){
-                queue.add(c+"");
-            }else if (c == '+' || c == '-'){
-                queue.add(pre+"");
-                queue.add(c+"");
-                pre = 0;
-            }else {
-                pre *= 10;
-                pre += c-'0';
-                if(i == s.length()-1) queue.add(pre+"");
+        Stack<Integer> numStack = new Stack<>();
+        Stack<Character> opStack = new Stack<>();
+        char op = ' ';
+        int cur = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch <= '9' && ch >= '0') {
+                cur = cur * 10 + (ch - '0');
+            } else if (ch == '+' || ch == '-') {
+                if (op == ' ') {
+                    numStack.add(cur);
+                } else {
+                    int pre = numStack.pop();
+                    if (op == '+') {
+                        cur += pre;
+                    } else {
+                        cur = pre - cur;
+                    }
+                    numStack.push(cur);
+                }
+                cur = 0;
+                op = ch;
+            } else if (ch == '(') {
+                if (op != ' ') {
+                    opStack.push(op);
+                }
+                opStack.push(ch);
+                op = ' ';
+            } else {
+                if (op == '+') {
+                    int pre = numStack.pop();
+                    cur += pre;
+                } else if (op == '-') {
+                    int pre = numStack.pop();
+                    cur = pre - cur;
+                }
+                opStack.pop();
+                if (!opStack.isEmpty() && opStack.peek() != '(') {
+                    int pre = numStack.pop();
+                    op = opStack.pop();
+                    if (op == '+') {
+                        cur += pre;
+                    } else {
+                        cur = pre - cur;
+                    }
+                }
+                op = ' ';
             }
         }
-        String p = queue.get(0);
-        if(p.equals("(")){
-            queue.remove(queue.size()-1);
-            queue.remove(0);
-            p = queue.get(0);
+        if (!numStack.isEmpty()) {
+            int pre = numStack.pop();
+            if (op == '+') {
+                cur += pre;
+            } else {
+                cur = pre - cur;
+            }
         }
-
-        return pre;
+        return cur;
     }
 }
