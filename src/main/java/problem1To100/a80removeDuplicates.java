95d2ab1898b674c36bd39fa4661463a99d8c36d9
liuwenhao <liuwenhao@agree.com>
2021-04-06 20:23:32 +0800

    0406

--- src/main/java/problem1To100/a80removeDuplicates.java
+++ src/main/java/problem1To100/a80removeDuplicates.java
@@ -2,20 +2,20 @@ package problem1To100;
 
 import java.util.Arrays;
 
+// 一年前能通过的代码一年后报错了。。。。
 public class a80removeDuplicates {
     public int removeDuplicates(int[] nums) {
-        int dupSize = 0,size=nums.length;
-        if(size < 2)    return size;
-        for(int i=0; i<nums.length-2;i++){
-            if(nums[i]>nums[i+1]) break;
-            if(nums[i] == nums[i+1] && nums[i] == nums[i+2]){
-                if (size - dupSize - 1 - i + 2 >= 0)
-                    System.arraycopy(nums, i + 2 + 1, nums, i + 2, size - dupSize - 1 - i + 2);
+        int dupSize = 0, size = nums.length;
+        if (size < 2) return size;
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > nums[i + 1]) break;
+            if (nums[i] == nums[i + 1] && nums[i] == nums[i + 2]) {
+                System.arraycopy(nums, i + 2 + 1, nums, i + 2, size - i - 3);
+                nums[size - dupSize - 1] = -1;
                 i--;
-                nums[size-dupSize-1] = -1;
                 dupSize++;
             }
         }
-        return size-dupSize;
+        return size - dupSize;
     }
 }
